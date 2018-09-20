2cc804291035c8b7ba66e27d9c1cf450176e1d67
User <User@192.168.1.102>
2018-09-20 14:57:40 +0200

    Versione internazionale

--- src/Prova.java
+++ src/Prova.java
@@ -13,7 +13,7 @@ public class Prova {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("ciao\n");
+		System.out.print("Welcome\n");
 	}
 
 }
